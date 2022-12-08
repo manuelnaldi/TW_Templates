9a406ae533c6a719510790a4301050a8575ea290
Manuel <mnaldi.business@gmail.com>
2022-12-08 17:06:35 +0100

    TW_Base

--- TW_Base/src/it/unibo/tw/web/servlets/StillAServlet.java
+++ TW_Base/src/it/unibo/tw/web/servlets/StillAServlet.java
@@ -7,19 +7,34 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class StillAServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	private String homeURL = null;
+	//variabili
+	String homeURL="";
 	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		this.homeURL = config.getInitParameter("homeURL");
 	}
+	
+	//risposte alle richieste GET
+	@Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        
+    }
 
+	//risposte alle richieste POST
+	@Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	
+	}
+	
+	
 	@Override
 	public void service(ServletRequest req, ServletResponse resp)
 	throws ServletException, IOException {
@@ -30,8 +45,11 @@ public class StillAServlet extends HttpServlet {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		// un altro forward eseguito lato servlet
 		req.getRequestDispatcher(homeURL).forward(req, resp);
 	}
+	
+	
 	
 }
