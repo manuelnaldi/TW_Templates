23ebf2fce5cb9a7ba3d169a5e74a3f0220c1ec87
Manuel <mnaldi.business@gmail.com>
2022-12-10 18:22:11 +0100

    update TW_Base

---
+++ TW_Base/src/beans/Bean.java
@@ -0,0 +1,24 @@
+package beans;
+
+import java.io.Serializable;
+
+public class Bean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	// --- Variabili -------
+	
+	
+	// --- constructor ----------
+	
+	public Bean() {}
+
+	// --- getters and setters --------------
+	
+	
+
+	// --- utilities ----------------------------
+	
+	
+
+}
---
+++ TW_Base/src/servlets/StillAServlet.java
@@ -0,0 +1,55 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class StillAServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	//variabili
+	String homeURL="";
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+	}
+	
+	//risposte alle richieste GET
+	@Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        
+    }
+
+	//risposte alle richieste POST
+	@Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	
+	}
+	
+	
+	@Override
+	public void service(ServletRequest req, ServletResponse resp)
+	throws ServletException, IOException {
+		// tempo di attesa, qui inserito artificiosamente
+		try {
+			Thread.sleep(5000);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// un altro forward eseguito lato servlet
+		req.getRequestDispatcher(homeURL).forward(req, resp);
+	}
+	
+	
+	
+}
